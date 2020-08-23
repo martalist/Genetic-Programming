a2bea82f4d12b0e5fc7279e96a79430f4dc7061d
Alistair Martin <alistairmartin01@gmail.com>
2020-08-23 20:16:06 +0800

    More function tests.

--- tests/FunctionTest.cpp
+++ tests/FunctionTest.cpp
@@ -75,8 +75,50 @@ namespace Tests
         ASSERT_DOUBLE_EQ(3.0, func->Evaluate());
 
         func = FunctionFactory::Create(FunctionType::Division);
-        func->AddChild(std::make_unique<Variable>(c));
-        func->AddChild(std::make_unique<Variable>(b));
+        ASSERT_TRUE(func->AddChild(std::make_unique<Variable>(c)));
+        ASSERT_TRUE(func->AddChild(std::make_unique<Variable>(b)));
         ASSERT_DOUBLE_EQ(1.5, func->Evaluate());
+
+        // division can only have 2 children
+        ASSERT_FALSE(func->AddChild(std::make_unique<Variable>(b)));
+    }
+
+    TEST_F(FunctionTest, SquareRoot)
+    {
+        auto func = FunctionFactory::Create(FunctionType::SquareRoot);
+        func->AddChild(std::make_unique<Variable>(a));
+        ASSERT_DOUBLE_EQ(1.0, func->Evaluate());
+
+        const double four = 4.0;
+        func = FunctionFactory::Create(FunctionType::SquareRoot);
+        ASSERT_TRUE(func->AddChild(std::make_unique<Variable>(four)));
+        ASSERT_DOUBLE_EQ(2.0, func->Evaluate());
+
+        // sqrt can only have one child
+        ASSERT_FALSE(func->AddChild(std::make_unique<Variable>(b)));
+    }
+
+    TEST_F(FunctionTest, CompositeFunction)
+    {
+
+        auto root = FunctionFactory::Create(FunctionType::SquareRoot);
+        auto div = FunctionFactory::Create(FunctionType::Division);
+        auto mult = FunctionFactory::Create(FunctionType::Multiplication);
+        auto add = FunctionFactory::Create(FunctionType::Addition);
+        auto sub = FunctionFactory::Create(FunctionType::Subtraction);
+
+        // (sqrt (/ (* b (+ a b c)) (- c b))) = sqrt(12)
+        add->AddChild(std::make_unique<Variable>(a));
+        add->AddChild(std::make_unique<Variable>(b));
+        add->AddChild(std::make_unique<Variable>(c));
+        sub->AddChild(std::make_unique<Variable>(c));
+        sub->AddChild(std::make_unique<Variable>(b));
+        mult->AddChild(std::make_unique<Variable>(b));
+        mult->AddChild(std::move(add));
+        div->AddChild(std::move(mult));
+        div->AddChild(std::move(sub));
+        root->AddChild(std::move(div));
+
+        ASSERT_DOUBLE_EQ(3.4641016151377544, root->Evaluate());
     }
 }
